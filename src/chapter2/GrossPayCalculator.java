505b0c0d39d0ba00e3cc0de87d70b6556bb1a06c
Clarke Newsham <Clarke.Newsham@yodel.co.uk>
2021-07-30 11:51:51 +0100

    Java Fundamentals Chapeter 2 learning path files and tutorials

---
+++ src/chapter2/GrossPayCalculator.java
@@ -0,0 +1,34 @@
+package chapter2;
+
+import java.util.Scanner;
+
+/**
+ * Program to calculate gross pay - Java Programming Chapter 2
+ */
+
+public class GrossPayCalculator {
+
+    public static void main(String[] args) {
+
+        //  1. Get the number of hours worked
+        System.out.println("Enter the number of hours the employee works");
+        Scanner scanner = new Scanner(System.in);
+        int hours = scanner.nextInt();
+
+        // 2. Get the hourly pay rate
+        System.out.println("Enter the employee payrate");
+        double rate = scanner.nextDouble();
+        scanner.close();
+
+        // 3. Multiply hours and pay rate
+        double grossPay = hours * rate;
+
+        // 4. Display result
+        System.out.println("The emplyees gross pay is $" + grossPay);
+
+
+
+
+    }
+
+}
