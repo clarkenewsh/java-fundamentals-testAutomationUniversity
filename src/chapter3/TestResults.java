6b88750f39394a0bc4634eca4e863303629d7941
Clarke Newsham <Clarke.Newsham@yodel.co.uk>
2021-08-16 17:01:46 +0100

    Chapter 3d - If else if statements

---
+++ src/chapter3/TestResults.java
@@ -0,0 +1,37 @@
+package chapter3;
+
+import java.util.Scanner;
+
+public class TestResults {
+
+    public static void main(String[] args) {
+
+        // Get test score from user
+        System.out.println("Please enter your test score:");
+        Scanner scanner = new Scanner(System.in);
+
+        // Store input into score variable
+        double score = scanner.nextDouble();
+
+        // Determine the letter grading for result
+        char grade;
+        if (score < 60){
+            grade = 'F';
+        }
+        else if(score < 70){
+            grade = 'D';
+        }
+        else if(score < 80) {
+            grade = 'C';
+        }
+        else if(score < 90) {
+            grade = 'B';
+        }
+        else {
+            grade = 'A';
+        }
+
+        System.out.println("Your grade is " + grade);
+
+    }
+}
